2827d22883c450ed69148bfe0c88540b918d1785
Gustavo Carrillo <g.yokebed@gmail.com>
2024-03-21 17:50:26 -0600

    Add product detail API

--- src/handlers/responseTransformer.js
+++ src/handlers/responseTransformer.js
@@ -1,28 +1,52 @@
-import mung from 'express-mung'
- 
-const transform = (body, req, res) => {
-    const response  = {
-      author: {
-        name: 'Gustavo',
-        lastName: 'Carrillo'
-      },
-      categories: body.available_filters.find(filter => filter.id === 'category')?.values.map((cat) => cat.name),
-      items: body.results.map(item => ({
+import mung from 'express-mung';
+
+const author = {
+  name: 'Gustavo',
+  lastName: 'Carrillo',
+}
+
+const transform = (body, req) => {
+
+  if (req.path == '/api/items') {
+    return {
+      author,
+      categories: body.available_filters
+        .find((filter) => filter.id === 'category')
+        ?.values.map((cat) => cat.name),
+      items: body.results.map((item) => ({
         id: item.id,
         title: item.title,
         price: {
           currency: item.currency_id,
           amount: item.price,
-          // decimals: TODO 
+          // decimals: TODO
         },
         picture: item.thumbnail,
         condition: item.condition,
-        free_shipping: item.shipping.free_shipping
-      }))
-    } 
-    return response;
-}
+        free_shipping: item.shipping.free_shipping,
+      })),
+    };
+  }
 
-export default mung.json(transform)
+  return {
+    author,
+    item: {
+      id: body.item.id,
+      title: body.item.title,
+      price: {
+        currency: body.item.currency_id,
+        amount: body.item.price,
+        // decimals: TODO
+      },
+      picture: body.item.pictures.find(
+        (picture) => picture.id === body.item.thumbnail_id
+      ).url,
+      condition: body.item.condition,
+      free_shipping: body.item.shipping.free_shipping,
+      solid_quantity: body.item.initial_quantity,
+      description: body.description.plain_text,
+    },
+  };
+};
 
-
+export default mung.json(transform);
--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import axios from 'axios';
-import morgan from 'morgan'
-import responseTransformer from './handlers/responseTransformer.js'
+import morgan from 'morgan';
+import responseTransformer from './handlers/responseTransformer.js';
 
 const app = express();
 
 app.use(responseTransformer);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   next();
 });
 
 if (app.get('env') === 'development') {
-  app.use(morgan('tiny'))
+  app.use(morgan('tiny'));
   console.log('Morgan enabled...');
 }
 
@@ -28,12 +28,39 @@ const request = async (search) => {
   }
 };
 
+const productDetailRequest = async (id) => {
+  const baseUrl = 'https://api.mercadolibre.com/items';
+
+  let endpoints = [`${baseUrl}/${id}`, `${baseUrl}/${id}/description`];
+
+  try {
+    return await Promise.all(endpoints.map((endpoint) => axios.get(endpoint)));
+  } catch (error) {
+    return error;
+  }
+};
+
 app.get('/api/items', (req, res) => {
   (async () => {
     try {
       const { data } = await request(req.query.q);
 
       res.json(data);
+    } catch (error) {
+      return error;
+    }
+  })();
+});
+
+app.get('/api/items/:id', (req, res) => {
+  (async () => {
+    try {
+      const [detail, description] = await productDetailRequest(req.params.id);
+
+      res.json({
+        item: { ...detail.data },
+        description: { ...description.data },
+      });
     } catch (error) {
       return error;
     }

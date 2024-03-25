3c0dcaf7d075905d606d5ec465c2907ad34ace34
Gustavo Carrillo <g.yokebed@gmail.com>
2024-03-24 23:16:47 -0600

    Allow CORS *

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(responseTransformer);
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Origin', "*");
   next();
 });
 
